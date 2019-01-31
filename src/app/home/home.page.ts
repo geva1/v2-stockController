5350c738db7444b583972c5da04e35eb8dda116f
Cleici <cleicilopes@hotmail.com>
2019-01-31 11:31:24 -0200

    feat(): tabs error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StockPage} from '../stock/stock.page';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
+}
+
+@Component({
+    templateUrl: 'home.page.html'
+
+})
+
+export class HomeTabs {
+
+    tab1Root = StockPage;
+    tab2Root = StockPage;
+    tab3Root = StockPage;
+
+    constructor() {
+
+    }
 }
